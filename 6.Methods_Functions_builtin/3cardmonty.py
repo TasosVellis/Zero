37d799eecfc516e2d0cdb394c4e23d0c5fccec3a
TasosVellis <anastasios.vellis@gmail.com>
2020-10-17 17:34:00 +0300

    Function Interaction,3 Card Monty

---
+++ 6.Methods_Functions_builtin/3cardmonty.py
@@ -0,0 +1,39 @@
+from random import shuffle
+
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input("Pick a number :0, 1 or 2 ")
+    return int(guess)
+
+
+def check_guess(mylist,guess):
+    if mylist[guess] == "O":
+        print("Correct")
+    else:
+        print("Wrong guess")
+        print(mylist)
+
+# Initial list
+
+
+my_list = [' ', 'O', ' ']
+
+
+# Shuffle list
+
+mixedup_list = shuffle_list(my_list)
+
+# User guess
+
+guess = player_guess()
+
+# Check guess
+
+check_guess(mixedup_list, guess)
