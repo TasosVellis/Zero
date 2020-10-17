a9623ad7fe3c8d311e5b481a3fb2e8d2f0ed183c
TasosVellis <anastasios.vellis@gmail.com>
2020-10-17 13:42:52 +0300

    Check

---
+++ 6.Methods_Functions_builtin/Functions.py
@@ -0,0 +1,17 @@
+def name_of_function(name):
+	'''
+	This is the docstring
+	'''
+	print("Hello "+name)
+
+name_of_function("Jose")
+
+
+def say_hello(name="Tasos"):
+	print(f'Hello {name}')
+
+say_hello('Angie')
+
+def sum_numbers(num1, num2):
+	return num1 + num2
+
